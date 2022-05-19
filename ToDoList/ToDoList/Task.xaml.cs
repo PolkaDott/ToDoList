d31eb824d35296637888b229b5c7340b23a7afa1
PolkaDott <andrey.gurt@yandex.ru>
2022-05-19 21:05:03 +0500

    добавлена разметка

--- ToDoList/ToDoList/Task.xaml.cs
+++ ToDoList/ToDoList/Task.xaml.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ToDoList
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Task : ContentView
     {
-        public MainPage()
+        public Task()
         {
             InitializeComponent();
-
         }
     }
 }
