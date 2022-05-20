95fabfe36093ce9edf0c7937f89f21609bcff505
PolkaDott <andrey.gurt@yandex.ru>
2022-05-20 21:13:27 +0500

    Изменены стили некоторых элементов

--- ToDoList/ToDoList.Android/MainActivity.cs
+++ ToDoList/ToDoList.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace ToDoList.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 128, 132, 219));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
